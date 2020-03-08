20ce03476e26067c50c265db93e7ec57b25a2887
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-03-08 15:50:46 -0500

    setting up environment

---
+++ src/main/java/com/filestacktests/demo/controllers/landingController.java
@@ -0,0 +1,16 @@
+package com.filestacktests.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class landingController {
+    @GetMapping("/home")
+    public String home(){
+        return "landing";
+    }
+
+    @GetMapping("/")
+    public String home2(){return "landing";}
+}
